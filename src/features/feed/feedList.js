d8e328e73a7815ebeca573dc4760454031dc1a0d
SIWIITHI <siwiphorn.thi@gmail.com>
2019-03-12 23:31:51 +0700

    First commit

---
+++ src/features/feed/feedList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class FeedList extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
